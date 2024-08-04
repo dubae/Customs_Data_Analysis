63e6d8808680b0ccc4ef6073a3641ee479a35949
SimonCreater <dongcheola3@gmail.com>
2024-08-04 13:05:36 +0900

    data

--- tax.py
+++ tax.py
@@ -4,3 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
 
+data = pd.read_csv('incomeoutcome.csv')
+print(data.columns)
+columns_to_check = ['수출 중량', '수출 금액', '수입 중량', '수입 금액', '무역수지']
+data_cleaned = data[~(data[columns_to_check] == 0).all(axis=1)]
+print(data_cleaned)
+data_cleaned.to_csv('data.csv', index=False,encoding='utf-8-sig')
